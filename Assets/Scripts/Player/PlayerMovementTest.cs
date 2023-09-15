aded43bbbcbfe6ae8fe324b124444c82dbb27590
yamilcasarreal <yamilcasarreal@gmail.com>
2023-09-14 21:03:41 -0400

    added comment

--- Assets/Scripts/Player/PlayerMovementTest.cs
+++ Assets/Scripts/Player/PlayerMovementTest.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PlayerMovementTest : MonoBehaviour
 {
+    //gjgijsgijgioshfisjgn
     public bool CanMove { get; private set; } = true;
     private bool isSprinting => canSprint && Input.GetKey(sprintKey);
     private bool ShouldJump => Input.GetKey(jumpKey) && characterController.isGrounded;
