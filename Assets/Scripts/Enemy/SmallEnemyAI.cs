2892d41b7b8c6d3e1cb0681212f66e1d364c3551
Gino Benitez <benitezgp81@gmail.com>
2023-10-09 18:39:15 -0400

    small enemy code
    start small enemy code left of at 7:43 in video

---
+++ Assets/Scripts/Enemy/SmallEnemyAI.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallEnemyAI : MonoBehaviour
+{
+    //NashMeshAgent variable
+    public NavMeshAgent ai;
+
+    //List variable that will contain a list of our destinations tranforms
+    public List<Transform> destinations;
+
+    //AI Animator variable
+    public Animator animate;
+
+    //float variables for walking speed, chase speed, idle, and destination amount
+    public float walkSpeed, chaseSpeed, minIdleTime, maxIdleTime, idelTime, destinationAmount;
+
+    //boolean variables to determine if enemy is walking or chasing
+    public bool walking, chasing;
+
+    //Transform variable for player
+
+    public Transform player;
+
+    //Transform for ai's current destination
+    Transform currentDest;
+
+    //Vector3 variable for AI's destination
+    Vector3 dest;
+
+    //int to randomize the AI's destinations
+    /*randNum2 will be used to randomize something when the AI
+    reaches its destination*/
+    int randNum, randNum2;
+
+    void Start()
+    {
+        walking = true;
+
+        /* randNum will equal to a random number from a random range of
+         numbers between 0 and destinationAmount*/
+        randNum = Random.Range(0, destinationAmount);
+
+        /*currentDest will equal to a destination from the destinations list
+         based on what the randNum equals to*/
+        currentDest = destinations[randNum];
+
+    }
+
+    void Update()
+    {
+        //if walking equals true, dest will equal to currentDest
+        if(walking == true)
+        {
+            dest = currentDest;
+
+            /*AI destination will equal to dest, meaning the AI will move 
+             toward dest*/
+            ai.destination = dest;
+
+            //AI speed will equal walkSpeed
+            ai.speed = walkSpeed;
+
+            /*if AI remaining distance is less than or equal to AI
+            stopping distance*/
+            if(ai.remainingDistance <= ai.stoppingDistance)
+            {
+                //randNum2 will equal a number between 0 and 1
+                randNum2 = Random.Range(0, 2);
+
+                if(randNum2 == 0)
+                {
+                    /*if randNum2 is equal to 0, the AI will pick a new 
+                    random destination and keep moving*/
+
+                    randNum = Random.Range(0, destinationAmount);
+                    currentDest = destinations[randNum];
+                }
+
+                if(randNum2 ==1)
+                {
+                    /*if randNum2 is equal to 1, AI will go idle and a 
+                     coroutine will start*/
+
+                    animate.ResetTrigger("walk");
+                    animate.ResetTrigger("idle");
+                    StopCoroutine("stayIdle");
+                    StartCoroutine("stayIdle");
+
+                    //walking will equal false
+                    walking = false;
+                }
+            }
+        }
+    }
+    IEnumerator stayIdle()
+    {
+        idleTime = Random.Range(minIdleTime, maxIdleTime);
+        yield return new WaitForSeconds(idleTime)
+    }
+}
