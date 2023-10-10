d6d6c2a8992e31a8819d1d3cff086acdc3e92667
Andy Henriquez <andyjh1991@yahoo.com>
2023-10-09 20:26:38 -0400

    Readded flashlight model and scripts

---
+++ Assets/Interact.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteract
+{
+    void Interaction();
+}
+
+public class Interact : MonoBehaviour
+{
+    public float interactDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int layerMask = 1 << 15;
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, interactDistance, layerMask))
+            {
+                var interactable = hit.transform.gameObject.GetComponent<IInteract>();
+                interactable.Interaction();
+            }
+
+        }
+    }
+}
